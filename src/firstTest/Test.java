f36a78c43d43df6b552247486fcb57b8335fb1bd
jeremywuhan <1039057279@qq.com>
2017-05-19 11:34:50 +0800

    Update Test.java

--- src/firstTest/Test.java
+++ src/firstTest/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          System.out.println("this is my first project");
+          System.out.println("this is the last project");
 	}
 
 }
